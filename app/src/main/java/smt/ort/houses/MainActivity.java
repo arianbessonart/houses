b30bfcfe415a235a301556e6c8c8c268b367bb3e
arianbessonart <arianbessonart@gmail.com>
2018-10-23 12:51:34 -0300

    add toolbar, drawer, navigation view

--- app/src/main/java/smt/ort/houses/MainActivity.java
+++ app/src/main/java/smt/ort/houses/MainActivity.java
@@ -3,12 +3,17 @@ package smt.ort.houses;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.List;
 
@@ -19,6 +24,8 @@ import smt.ort.houses.ui.HouseViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawer;
+    NavigationView navigationView;
     private HouseViewModel houseViewModel;
 
     @Override
@@ -27,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        setupDrawer(navigationView);
 
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
@@ -49,5 +64,15 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    private void setupDrawer(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+//                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
     }
 }
