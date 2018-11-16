9d4abc5a0e2308bac9a027b991782477fc4f72dd
arianbessonart <arianbessonart@gmail.com>
2018-11-16 12:42:07 -0300

    refactor code

--- app/src/main/java/smt/ort/houses/MainActivity.java
+++ app/src/main/java/smt/ort/houses/MainActivity.java
@@ -84,12 +84,6 @@ public class MainActivity extends AppCompatActivity implements HomeFragment.OnHo
         drawer.closeDrawers();
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
     private void setupDrawer(NavigationView navigationView) {
         navigationView.setNavigationItemSelectedListener(menuItem -> {
             selectDrawerItem(menuItem);
